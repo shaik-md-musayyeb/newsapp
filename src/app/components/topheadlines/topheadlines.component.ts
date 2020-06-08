be31beefcd42d7c719c6ea8f9fbd14f74cb4521f
shaik-md-musayyeb <smd52952@gmail.com>
2020-06-08 14:13:42 +0530

    first commit

---
+++ src/app/components/topheadlines/topheadlines.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-topheadlines',
+  templateUrl: './topheadlines.component.html',
+  styleUrls: ['./topheadlines.component.css']
+})
+export class TopheadlinesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
